5b729a6cd5a4174940b301fe26ed23f380ad0e91
Pablo C. Ruíz <pcruiher089@gmail.com>
2020-09-11 22:20:59 -0500

    done

--- DynamicCalculator/utils.js
+++ DynamicCalculator/utils.js
@@ -1,24 +1,43 @@
-function get_element_li (name, price) {
-    return `<li class="added-item">name: ${name} price: ${price}  <button class="remove-item">remove</button></li>`
+document.addEventListener('DOMContentLoaded', () => new page());
+
+let itemName = document.querySelector('#item-name');
+let itemValue = document.querySelector('#item-value');
+let form = document.querySelector('#container');
+let items = document.querySelector('#items');
+let total = document.querySelector('#total');
+
+class page {
+  constructor() {
+    this.itemName = itemName;
+    this.itemValue = itemValue;
+    this.form = form;
+    this.items = items;
+    this.total = total;
+    this.form.addEventListener('submit', this.addItem);
+    document.body.addEventListener('click',(event)=>{
+      if (event.target.classList.contains('delete-item')){
+        const element = event.target.parentElement;
+        const total = Number(this.total.innerText) - Number(element.name);
+        this.total.innerText = total;
+        element.remove();
+      }
+    });
   }
-  
-  let add_item_to_list_with_template = (template_function) => {
-    return (event) => {
-      /*
-        add the item to the list
-        add event listener to the button inside the element just added with the remove_item function
-        add the value to the total
-      */
+
+  addItem = (event) => {
+    event.preventDefault();
+    if(this.itemName.value == "" || this.itemValue.value == "" || isNaN(this.itemValue.value) || Number(this.itemValue.value < 0)){
+      this.form.style = 'border: 10px solid red';
+      return;
     }
-  }
-  /*
-   for removing elements could be this way
-    let element_to_delete = document.querySelector("selector").lastElementChild;
-    element_to_delete.parentNode.removeChild(element_to_delete);
-    or we could use ChildNode.remove()
-    https://developer.mozilla.org/en-US/docs/Web/API/ChildNode/remove
-  */
-  
-  let remove_item  = (node_to_remove) => {
-  
-  }
+    const element = document.createElement('div');
+    element.append(`name: ${this.itemName.value} price: ${this.itemValue.value}`);
+    element.name = this.itemValue.value;
+    const deleteButton = document.createElement('button');
+    deleteButton.innerText = 'delete';
+    deleteButton.classList.add('delete-item');
+    element.appendChild(deleteButton);
+    this.items.appendChild(element);
+    this.total.innerText = Number(this.itemValue.value) + Number(this.total.innerText);
+  };
+}
